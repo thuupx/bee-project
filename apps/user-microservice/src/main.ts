df2201e06c44b13a3138e58b3caf4ea76d83543f
thuupx <xuanthu404@gmail.com>
2023-08-30 18:05:05 +0700

    fix: change to using kafka for communicate

--- apps/user-microservice/src/main.ts
+++ apps/user-microservice/src/main.ts
@@ -1,27 +1,20 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { KafkaOptions, Transport } from '@nestjs/microservices';
 
 import { AppModule } from './app/app.module';
-import { TcpOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const port: number = parseInt(process.env.PORT) || 4002;
-
-  const app = await NestFactory.createMicroservice<TcpOptions>(AppModule, {
-    transport: Transport.TCP,
+  const app = await NestFactory.createMicroservice<KafkaOptions>(AppModule, {
+    transport: Transport.KAFKA,
     options: {
-      port,
+      client: {
+        brokers: ['localhost:9094'],
+      },
     },
   });
   await app.listen();
-  Logger.log(
-    `🚀 User microservice is running on: http://localhost:${port}}`
-  );
+  Logger.log(`🚀 User microservice is running`, 'UserMicroservice');
 }
 
 bootstrap();
