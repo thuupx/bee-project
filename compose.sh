1d9aa338cff2ceddc6e2b8db944e351e722bcbbd
thuupx <xuanthu404@gmail.com>
2023-09-25 15:42:17 +0700

    fix: separate docker compose

--- compose.sh
+++ compose.sh
@@ -1,1 +1,2 @@
-docker-compose -f docker-compose.yml -f kong.docker-compose.yml up -d
+docker-compose --profile kong --env-file .env  -f api-gateway.docker-compose.yml up -d --build &&
+docker-compose -f docker-compose.yml up -d
