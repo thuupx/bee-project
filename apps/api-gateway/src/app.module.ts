eff4c55c409437b728b92327bf01f51f6d27bd40
thuupx <xuanthu404@gmail.com>
2023-10-06 15:57:40 +0700

    chore: migrate api gateway to kong

--- apps/api-gateway/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import {ConfigModule} from '@bee-project/infrastructure'
-import {Module} from '@nestjs/common'
-
-import {AccountModule} from './microservices/account/account.module'
-
-@Module({
-  imports: [ConfigModule.register(), AccountModule],
-})
-export class AppModule {}
